51a101438d71595178bae13ada25434ce4d1c627
João Wojciechowski <55636585+JoaoWoj@users.noreply.github.com>
2021-04-03 19:09:10 -0300

    Fazendo a criação de todas chamadas no controller

--- src/main/java/com/api/controllers/apiController.java
+++ src/main/java/com/api/controllers/apiController.java
@@ -1,6 +1,7 @@
 package com.api.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class apiController {
 	
+// Teste para ver se chamadas da API estão funcionando	
 	@GetMapping("/")
-	public String testeChamada() {
+	public String testeChamadaAPI() {
 		return "OK!";
+	}
+	
+// Problema 01	
+	@GetMapping("/listarTickets")
+	public String listarTickets() {
+		return "Listar Tickets Ok!";
+	}
+	
+	@PostMapping("/criarTicket")
+	public String criarTicket() {
+		return "Criar Ticket Ok!";
+	}
+	
+// Problema 02
+	@GetMapping("/receita")
+	public String listarReceita() {
+		return "Listar Receita Ok!";
 	}
 
 }
