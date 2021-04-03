6f20180507df2f3ff741ffeef95c97bca8b5acd5
João Wojciechowski <55636585+JoaoWoj@users.noreply.github.com>
2021-04-03 19:09:40 -0300

    Criando os models

---
+++ src/main/java/com/api/models/TicketModel.java
@@ -0,0 +1,67 @@
+package com.api.models;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity(name = "ticket")
+public class TicketModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String titulo;
+	
+	@ManyToOne
+    @JoinColumn(name = "codcliente")
+	private ClienteModel cliente;
+	
+	@ManyToOne
+    @JoinColumn(name = "codmodulo")
+	private ModuloModel modulo;
+	
+	private Date dataAbertura;
+	private Date dataEncerramento;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public ClienteModel getCliente() {
+		return cliente;
+	}
+	public void setCliente(ClienteModel cliente) {
+		this.cliente = cliente;
+	}
+	public ModuloModel getModulo() {
+		return modulo;
+	}
+	public void setModulo(ModuloModel modulo) {
+		this.modulo = modulo;
+	}
+	public Date getDataAbertura() {
+		return dataAbertura;
+	}
+	public void setDataAbertura(Date dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
+	public Date getDataEncerramento() {
+		return dataEncerramento;
+	}
+	public void setDataEncerramento(Date dataEncerramento) {
+		this.dataEncerramento = dataEncerramento;
+	}
+}
