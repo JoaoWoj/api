c506a3e9d2202d06512e3f0dae26525799de9dd8
João Wojciechowski <55636585+JoaoWoj@users.noreply.github.com>
2021-04-04 00:40:09 -0300

    Fazendo a criação da requisição do problema 2

--- src/main/java/com/api/controllers/APIController.java
+++ src/main/java/com/api/controllers/APIController.java
@@ -1,13 +1,27 @@
 package com.api.controllers;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.api.models.TicketModel;
+import com.api.repository.TicketRepository;
+
 @RestController
 @RequestMapping("/api")
 public class APIController {
+	
+	@Autowired
+	private TicketRepository ticketRepository;
 	
 // Teste para ver se chamadas da API estão funcionando	
 	@GetMapping("/")
@@ -17,8 +31,8 @@ public class APIController {
 	
 // Problema 01	
 	@GetMapping("/listarTickets")
-	public String listarTickets() {
-		return "Listar Tickets Ok!";
+	public List<TicketModel> listarTickets() {
+		return ticketRepository.findAll();
 	}
 	
 	@PostMapping("/criarTicket")
@@ -27,9 +41,33 @@ public class APIController {
 	}
 	
 // Problema 02
-	@GetMapping("/receita")
-	public String listarReceita() {
-		return "Listar Receita Ok!";
+	@GetMapping("/receita/prato={nomePrato}")
+	public String listarReceita(@PathVariable String nomePrato) {
+		try {
+			String url = "https://forkify-api.herokuapp.com/api/search?q=" + nomePrato;
+			HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+			
+			if (conn.getResponseCode() != 200) {
+				return "Prato não encontrado!";
+			 }
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+            String output = "";
+            String line;
+            while ((line = br.readLine()) != null) {
+                output += line;
+            }
+
+            conn.disconnect();
+			
+			return new String(output.getBytes());
+		} catch (Exception e) {
+			return "Ocorreu um erro durante a execução: " + e.getMessage();
+		}
 	}
 
 }
